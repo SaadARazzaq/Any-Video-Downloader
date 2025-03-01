6b0782d1f03ec7e1e569460ae5f6eee6a73ca03e
Saad Abdur Razzaq <123338307+SaadARazzaq@users.noreply.github.com>
2025-03-01 22:51:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ import yt_dlp
 import os
 import tempfile
 
-def download_video(url, filename):
-
+def download_video(url):
     temp_dir = tempfile.gettempdir()
-    video_path = os.path.join(temp_dir, f"{filename}.mp4")
-
+    
     ydl_opts = {
         'format': 'best',
-        'outtmpl': video_path,  
+        'outtmpl': os.path.join(temp_dir, "%(title)s.%(ext)s"),  
         'noplaylist': True,
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -20,30 +18,30 @@ def download_video(url, filename):
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        info = ydl.extract_info(url, download=True)
+        video_path = os.path.join(temp_dir, f"{info['title']}.{info['ext']}")
 
     return video_path  
 
 st.title("~ ANY Video Downloader (This does not support carousels ❌)")
 
 video_url = st.text_input("Enter the video URL:")
-custom_filename = st.text_input("Enter the desired filename (without extension):")
 
 if st.button("Download Video"):
-    if video_url and custom_filename:
+    if video_url:
         st.info("Downloading... Please wait.")
         try:
-            video_path = download_video(video_url, custom_filename)
+            video_path = download_video(video_url)
             st.success("Download Complete! Click below (👇) to save the file.")
 
             with open(video_path, "rb") as file:
                 st.download_button(
                     label="Download Video",
                     data=file,
-                    file_name=f"{custom_filename}.mp4",
+                    file_name=os.path.basename(video_path),
                     mime="video/mp4"
                 )
         except Exception as e:
             st.error(f"Error: {e}")
     else:
-        st.warning("Please enter both a valid URL and a filename.")
+        st.warning("Please enter a valid URL.")
